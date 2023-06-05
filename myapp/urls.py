c8ab2cda3fd532453126c5871636ae897ebe2aa8
Wayfarerdesert <facundo355@outlook.com>
2023-06-05 17:31:49 +0200

    Jinja, Templates inheritance, URLs y redirect ok

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
-    path('about/', views.about),
-    path('hello/<str:username>', views.hello),
-    path('projects/', views.projects),
-    path('tasks/', views.tasks),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('hello/<str:username>', views.hello, name='hello'),
+    path('projects/', views.projects, name='projects'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('createTask/', views.createTask, name='createTasks'),
+    path('createProject/', views.createProject, name='createProject'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,27 +1,67 @@
 # from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import CreateNewTask, CreateNewProject
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    title = 'Django Course!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
 
 def about(request):
-    return render(request, 'about.html')
+    username = 'Oso'
+    return render(request, 'about.html', {
+        'username': username
+    })
 
 def hello(request, username):
     print(username)
     return HttpResponse('<h1>Hello %s</h1>' % username)
 
 def projects(request):
-    projects = list(Project.objects.values())
+    # projects = list(Project.objects.values())
+    projects = Project.objects.all()
     # return JsonResponse(projects, safe=False)
-    return render(request, 'projects.html')
+    return render(request, 'projects/projects.html', {
+        'projects': projects
+    })
 
 def tasks(request):
     # task = Task.objects.get(id=id)
     # task = get_object_or_404(Task, id=id)
+    tasks = Task.objects.all()
     # return HttpResponse('task: %s' % task.title)
-    return render(request, 'tasks.html')
+    return render(request, 'tasks/tasks.html', {
+        'tasks': tasks
+    })
+
+def createTask(request):
+    if request.method == 'GET':
+        # show interface
+        return render(request, 'tasks/createTask.html', {
+            'form': CreateNewTask()
+        })
+    else:
+        Task.objects.create(title=request.POST['title'], description=request.POST['description'], projectkey=1)
+        return redirect('/tasks/')
+
+def createProject(request):
+    if request.method == 'GET':
+        return render(request, 'projects/createProject.html', {
+        'form': CreateNewProject()
+    })
+    else:
+        # print(request.POST)
+        Project.objects.create(name=request.POST['name'])
+        redirect('projects')
+    #     print(project)
+    #     return render(request, 'projects/createProject.html', {
+    #     'form': CreateNewProject()
+    # })
+        
+
+
 
