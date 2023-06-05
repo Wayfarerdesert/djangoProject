7aba36df5056129a5d99ff55eaf52089a4540a3b
Wayfarerdesert <facundo355@outlook.com>
2023-06-05 13:30:03 +0200

    Render y Templates

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.hello),
-    path('about/', views.about)
+    path('', views.index),
+    path('about/', views.about),
+    path('hello/<str:username>', views.hello),
+    path('projects/', views.projects),
+    path('tasks/', views.tasks),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,27 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('<h1>Hello World</h1>')
+def index(request):
+    return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse('About')
+    return render(request, 'about.html')
+
+def hello(request, username):
+    print(username)
+    return HttpResponse('<h1>Hello %s</h1>' % username)
+
+def projects(request):
+    projects = list(Project.objects.values())
+    # return JsonResponse(projects, safe=False)
+    return render(request, 'projects.html')
+
+def tasks(request):
+    # task = Task.objects.get(id=id)
+    # task = get_object_or_404(Task, id=id)
+    # return HttpResponse('task: %s' % task.title)
+    return render(request, 'tasks.html')
+
